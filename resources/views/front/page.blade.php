1abefce1834e581ad01cf85eb6aec3fad23390b1
Friska Fikianti <120500464+friskafikianti@users.noreply.github.com>
2024-06-19 15:31:06 +0700

    Update page.blade.php

--- resources/views/front/page.blade.php
+++ resources/views/front/page.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="light-font">
                 <ol class="breadcrumb primary-color mb-0">
-                    <li class="breadcrumb-item"><a class="white-text" href="{{ route('front.home') }}">Home</a></li>
+                    <li class="breadcrumb-item"><a class="white-text" href="{{ route('front.home') }}">KuyBelanja</a></li>
                     <li class="breadcrumb-item">{{ $page->name }}</li>
                 </ol>
             </div>
